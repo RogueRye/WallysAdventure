9fb500c04d8ea84a4b5daace3123cc194ab5f718
RogueRye <31779039+RogueRye@users.noreply.github.com>
2018-10-18 15:14:16 -0400

    Character Controller Started

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+    public GameObject graphics;
+    public CameraFollow camController;
+
+
+    [Header("Stats")]
+    public float jumpForce = 6;
+
+
+    [Header("Info")]
+    public Transform groundPoint;
+    [Range(0, 1)]
+    public float radius;
+    public LayerMask groundMask;
+
+
+    public bool grounded = false;
+    bool jumpInput;
+    Rigidbody2D rb;
+    Animator anim;
+    private bool isSprinting;
+    private float moveSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = graphics.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        grounded = Physics2D.OverlapCircle(groundPoint.position, radius, groundMask);
+    }
+
+
+    private void Move()
+    {
+
+        float inputH = Input.GetAxisRaw("Horizontal");
+
+        Vector2 moveDir =  new Vector2(inputH * moveSpeed, rb.velocity.y);
+        rb.velocity = moveDir;
+
+
+
+        if (moveDir.x != 0)
+        {
+            anim.SetBool("isMoving", true);
+            Vector3 lookDir;
+            if (moveDir.x > 0)
+            {
+                lookDir = Vector2.right;
+            }
+            else if (moveDir.x < 0)
+            {
+                lookDir = Vector2.left;
+            }
+
+
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+
+
+
+    }
 }
